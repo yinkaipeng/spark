2295fbe1d029e15de01f01c250c15bf1c0c17c50
Zhan Zhang <zhazhan@gmail.com>
2015-01-27 13:45:22 -0800

    system test facility

---
+++ examples/src/main/scala/org/apache/spark/examples/WordCount.scala
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.examples
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext._
+
+object WordCount {
+  /** Usage: HdfsTest [file] */
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: WordCount <input> <output")
+      System.exit(1)
+    }
+    val sparkConf = new SparkConf().setAppName("WordCount")
+    val sc = new SparkContext(sparkConf)
+    val file = sc.textFile(args(0))
+    val counts = file.flatMap(line => line.split(" ")).map(word =>(word, 1)).reduceByKey(_ + _)
+    counts.saveAsTextFile(args(1))
+    sc.stop()
+  }
+}
