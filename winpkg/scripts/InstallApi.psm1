0723b334cb93896e31bfae3b946b7c58579ea691
ivanmalamen <malamen@ukr.net>
2015-06-09 13:39:50 +0300

    BUG-39073 Spark HistoryServer is not accessible in Windows

--- winpkg/scripts/InstallApi.psm1
+++ winpkg/scripts/InstallApi.psm1
@@ -140,6 +140,10 @@ function Install(
 		{
 			CreateAndConfigureHadoopService $service $HDP_RESOURCES_DIR $sparkInstallToBin $serviceCredential
             $cmd="$ENV:WINDIR\system32\sc.exe config $service start= disabled"
+            if ($service -eq "sparkhistoryserver")
+            {
+                $cmd="$ENV:WINDIR\system32\sc.exe config $service start= demand"
+            }
             Invoke-CmdChk $cmd
 			###
             ### Setup Spark service config
