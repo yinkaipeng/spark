81635862bf2be09904dbc5119f06ca0c968530b1
jerryshao <sshao@hortonworks.com>
2016-03-18 13:21:35 +0800

    Fix attempt id regression

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
@@ -133,12 +133,9 @@ private[spark] class ApplicationMaster(
         // Set the master property to match the requested mode.
         System.setProperty("spark.master", "yarn-cluster")
 
-        // Propagate the application ID so that YarnClusterSchedulerBackend can pick it up.
+        // Set this internal configuration if it is running on cluster mode, this
+        // configuration will be checked in SparkContext to avoid misuse of yarn cluster mode.
         System.setProperty("spark.yarn.app.id", appAttemptId.getApplicationId().toString())
-
-        // Propagate the attempt if, so that in case of event logging,
-        // different attempt's logs gets created in different directory
-        System.setProperty("spark.yarn.app.attemptId", appAttemptId.getAttemptId().toString())
       }
 
       logInfo("ApplicationAttemptId: " + appAttemptId)
