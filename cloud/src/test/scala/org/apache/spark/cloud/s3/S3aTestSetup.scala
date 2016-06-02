aaeede5d08ac5a932e36ea17acf67cf39aa3391d
Steve Loughran <stevel@apache.org>
2016-06-02 19:34:29 +0100

    [SPARK-7481] add spark-cloud module

---
+++ cloud/src/test/scala/org/apache/spark/cloud/s3/S3aTestSetup.scala
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.cloud.s3
+
+import java.net.URI
+
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.s3a.Constants
+
+import org.apache.spark.cloud.CloudSuite
+
+/**
+ * Trait for S3A tests
+ */
+trait S3aTestSetup extends CloudSuite {
+
+  override def enabled: Boolean = super.enabled && conf.getBoolean(S3A_TESTS_ENABLED, false)
+
+  def initFS(): FileSystem = {
+    val id = requiredOption(AWS_ACCOUNT_ID)
+    val secret = requiredOption(AWS_ACCOUNT_SECRET)
+    conf.set("fs.s3n.awsAccessKeyId", id)
+    conf.set("fs.s3n.awsSecretAccessKey", secret)
+    conf.set(Constants.BUFFER_DIR, localTmpDir.getAbsolutePath)
+    val s3aURI = new URI(requiredOption(S3A_TEST_URI))
+    logDebug(s"Executing S3 tests against $s3aURI")
+    createFilesystem(s3aURI)
+  }
+}
